724bcf75ffbee32f1c599c69ad2eb6e2afa51f27
MichaelZ <michael.zinsmaier@uni-konstanz.de>
2011-12-12 22:10:50 +0100

    just the hull

---
+++ renderer/states/VisAdjusting.hpp
@@ -0,0 +1,34 @@
+#ifndef VIS_ADJUSTING_H
+#define VIS_ADJUSTING_H
+
+#include "../IRenderState.hpp"
+#include "../../painter/VisPainter.hpp"
+
+class Renderer;
+
+class VisAdjusting : public IRenderState {
+
+	public:
+		VisAdjusting(Renderer* renderer);
+		~VisAdjusting( void );
+
+		//Interface methods
+		void render( void );
+		void renderGauss( void );
+		void renderEvalField( void );
+		void renderLineField( void );
+		void work( void );
+		void takeOver( void );
+		void changePanning(int xMouseMove, int yMouseMove);
+		void changeZoom( void );
+		void changeData(string nodeFile, string edgeFile="");
+		void changeSideLength( void );
+		void changeWindow( void );
+
+	private:
+		Renderer* _r;
+		VisPainter* _visPainter;
+};
+
+
+#endif
